a91235d481939b6c6105c2c926db4655cf7d26dc
Tudor Jude <tudor.jude47@gmail.com>
2020-07-29 20:40:31 +0300

    > initial commit

---
+++ Assets/_verticalShooter/Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject leftLimit;
+    public GameObject rightLimit;
+
+    [Header("Prefab Enemies")]
+    public Enemy AsteroidPrefab;
+
+    public float spawnFrequency = 3f;
+
+    private float ySpawnValue;
+
+    private float spawnCounter = 0f;
+    private float xLeftSpawnValue;
+    private float xRightSpawnValue;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ySpawnValue = leftLimit.transform.position.y;
+        xLeftSpawnValue = leftLimit.transform.position.x;
+        xRightSpawnValue = rightLimit.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnCounter += Time.deltaTime;
+        if(spawnCounter >= spawnFrequency)
+        {
+            spawnCounter = 0f;
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(xLeftSpawnValue, xRightSpawnValue), ySpawnValue, 0f);
+        Instantiate(AsteroidPrefab, spawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/_verticalShooter/Scripts/PlayerShip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mosePos2D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mosePos2D.x, mosePos2D.y, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision detected: " + collision.gameObject.name);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
